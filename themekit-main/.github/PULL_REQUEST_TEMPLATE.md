c5dfc490d3f19b310c90048db9e8c1e464ddd485
Pablo Olmo López <pabloolmolopez9@gmail.com>
2024-07-11 10:16:42 +0200

    ThemeKit

---
+++ themekit-main/.github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,7 @@
+
+
+### Warn Checklist
+- [ ] This changes the interface and requires a Major/Minor version change.
+- [ ] I have :tophat:'d these changes by using the commands I changed by hand.
+- [ ] I have added a dependency to the project.
+- [ ] I have considered any potential impact on [node-themekit](https://github.com/Shopify/node-themekit)
---
+++ themekit-main/choco/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageName    = $env:ChocolateyPackageName
+$file           = "$($toolsDir)\theme.exe"
+$version        = "v1.3.2"
+$url            = "https://shopify-themekit.s3.amazonaws.com/$($version)/windows-386/theme.exe"
+$url64          = "https://shopify-themekit.s3.amazonaws.com/$($version)/windows-amd64/theme.exe"
+$checksum       = 'd5777417bda8cf086c4103dd50891fc8'
+$checksum64     = 'b569ccf03d2e9c358a8f58744f9b265e'
+$validExitCodes = @(0)
+
+Get-ChocolateyWebFile `
+  -PackageName $packageName `
+  -FileFullPath $file `
+  -Url "$url" `
+  -Checksum $checksum `
+  -ChecksumType "md5" `
+  -Url64bit "$url64" `
+  -Checksum64 $checksum64 `
+  -ChecksumType64 "md5"
